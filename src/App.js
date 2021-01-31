66c38df73c8d39936d35ed82e68c3c5487db7ed7
Daniel Cortes Cespedes <daniel@dancortes.com>
2021-01-31 14:43:20 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import { CssBaseline } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => {
+  return createStyles({
+    bar: {
+      width: "100%",
+      "& > * + *": {
+        marginTop: theme.spacing(2),
+      },
+    },
+  });
+});
+
+export default () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <CssBaseline />
+      <div className={classes.bar}>
+        <LinearProgress />
+      </div>
+    </>
+  );
+};
